bbf8d69675746926a32e5200610b21d7b8b035f7
Anton Parshikov <tosha1703@gmail.com>
2023-06-09 10:00:13 +0300

    add fix in main

--- homework-1/main.py
+++ homework-1/main.py
@@ -7,32 +7,43 @@ connection = psycopg2.connect(
     user="postgres",
     password="324214Kross!"
 )
-# try:
-#     with connection:
-#         with connection.cursor() as cursor:
-#             with open('../homework-1/north_data/customers_data.csv',
-#                       'r', encoding="utf-8") as f:
-#                 next(f)
-#                 cursor.copy_from(f, 'customers', sep=',', null='')
-# finally:
-#     connection.close()
 
-try:
-    with connection:
-        with connection.cursor() as cursor:
-            with open('../homework-1/north_data/employees_data.csv',
-                      'r', encoding="utf-8") as f:
-                next(f)
-                cursor.copy_from(f, 'employees', sep=',', null='')
-finally:
-    connection.close()
-#
-# try:
-#     with connection:
-#         with connection.cursor() as cursor:
-#             with open('../homework-1/north_data/orders_data.csv',
-#                       'r', encoding="utf-8") as f:
-#                 next(f)
-#                 cursor.copy_from(f, 'orders', sep=',', null='')
-# finally:
-#     connection.close()
+
+def customers_add_data():
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                with open('../homework-1/north_data/customers_data.csv',
+                          'r', encoding="utf-8") as f:
+                    next(f)
+                    cursor.copy_from(f, 'customers', sep=',', null='')
+    finally:
+        connection.close()
+
+
+def employees_add_data():
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                with open('../homework-1/north_data/employees_data.csv',
+                          'r', encoding="utf-8") as f:
+                    next(f)
+                    cursor.copy_from(f, 'employees', sep=',', null='')
+    finally:
+        connection.close()
+
+
+def orders_add_data():
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                with open('../homework-1/north_data/orders_data.csv',
+                          'r', encoding="utf-8") as f:
+                    next(f)
+                    cursor.copy_from(f, 'orders', sep=',', null='')
+    finally:
+        connection.close()
+
+
+if __name__ == '__main__':
+    employees_add_data()
