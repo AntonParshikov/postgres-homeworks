1e4fc0fb67675a145d71d8308adefaccc0a65655
Anton Parshikov <tosha1703@gmail.com>
2023-06-11 14:29:26 +0300

    add fix in main

--- homework-1/main.py
+++ homework-1/main.py
@@ -11,59 +11,47 @@ connection = psycopg2.connect(
 
 
 def customers_add_data():
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                with open('north_data/customers_data.csv', 'r') as f:
-                    reader = csv.reader(f)
-                    next(reader)  # пропуск хедера
-                    for row in reader:
-                        cursor.execute(
-                            "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
-                            row
-                        )
-
-    finally:
-        connection.close()
+    with connection:
+        with connection.cursor() as cursor:
+            with open('north_data/customers_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # пропуск хедера
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                        row
+                    )
 
 
 def employees_add_data():
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                with open('north_data/employees_data.csv', 'r') as f:
-                    reader = csv.reader(f)
-                    next(reader)
-                    for row in reader:
-                        cursor.execute(
-                            "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) "
-                            "VALUES (%s, %s, %s, %s, %s, %s)",
-                            row
-                        )
-    finally:
-        connection.close()
+    with connection:
+        with connection.cursor() as cursor:
+            with open('north_data/employees_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) "
+                        "VALUES (%s, %s, %s, %s, %s, %s)",
+                        row
+                    )
 
 
 def orders_add_data():
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                with open('north_data/orders_data.csv', 'r') as f:
-                    reader = csv.reader(f)
-                    next(reader)  # пропуск хедера
-                    for row in reader:
-                        cursor.execute(
-                            "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) "
-                            "VALUES (%s, %s, %s, %s, %s)",
-                            row
-                        )
-
-    finally:
-        connection.close()
+    with connection:
+        with connection.cursor() as cursor:
+            with open('north_data/orders_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # пропуск хедера
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) "
+                        "VALUES (%s, %s, %s, %s, %s)",
+                        row
+                    )
 
 
 if __name__ == '__main__':
-    # employees_add_data()
-    # customers_add_data()
-    # orders_add_data()
-    pass
+    employees_add_data()
+    customers_add_data()
+    orders_add_data()
